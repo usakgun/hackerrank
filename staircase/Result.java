d4c187d29b1479322f4ae751ead300eacfd10f1f
Umut <149551428+usakgun@users.noreply.github.com>
2024-07-21 03:24:32 +0200

    Add files via upload

--- staircase/Result.java
+++ staircase/Result.java
@@ -1,1 +1,33 @@
+package staircase;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+    for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i+j<n-1) System.out.print(' ');
+                else System.out.print('#');
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
---
+++ staircase/Solution.java
@@ -0,0 +1,58 @@
+package staircase;
+/*
+Staircase detail
+
+This is a staircase of size n = 4:
+
+   #
+  ##
+ ###
+####
+Its base and height are both equal to n. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+
+Function Description
+
+Complete the staircase function in the editor below.
+
+staircase has the following parameter(s):
+
+int n: an integer
+Print
+
+Print a staircase as described above.
+
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
