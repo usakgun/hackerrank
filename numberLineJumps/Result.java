ab837c87a7399e5b88036a88e54cfb4ee7c76137
Umut <149551428+usakgun@users.noreply.github.com>
2024-07-21 03:42:20 +0200

    Add files via upload

--- numberLineJumps/Result.java
+++ numberLineJumps/Result.java
@@ -1,1 +1,40 @@
+package numberLineJumps;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+    String result = "NO";
+    for(int i=0;i<10000;i++){
+        x1 += v1;
+        x2 += v2;
+        if(x1 == x2){
+            result = "YES";
+        }
+    }
+    return result;
+    }
+
+}
---
+++ numberLineJumps/Solution.java
@@ -0,0 +1,69 @@
+package numberLineJumps;
+/*
+You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction 
+(i.e, toward positive infinity).
+
+The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
+
+Example
+x1 = 2
+v1 = 1
+x2 = 1
+v2 = 2
+
+
+
+After one jump, they are both at x = 3, so the answer is YES.
+
+Function Description
+
+Complete the function kangaroo in the editor below.
+
+kangaroo has the following parameter(s):
+
+int x1, int v1: starting position and jump distance for kangaroo 1
+int x2, int v2: starting position and jump distance for kangaroo 2
+Returns
+
+string: either YES or NO
+Input Format
+
+A single line of four space-separated integers denoting the respective values of x1, v1, x2, and v2. */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = Result.kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
