84316f94f5d7005f831a383fe5e6ba36352f09be
Umut <149551428+usakgun@users.noreply.github.com>
2024-07-21 03:30:53 +0200

    Add files via upload

--- birthdayCakeCandles/Result.java
+++ birthdayCakeCandles/Result.java
@@ -1,1 +1,42 @@
+package birthdayCakeCandles;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    Iterator it = candles.iterator();
+    int numoftallest = 0;
+    int currentTall = candles.get(0);
+    while(it.hasNext()){
+        if(Integer.parseInt(it.next().toString()) > currentTall){
+            currentTall = Integer.parseInt(it.next().toString());
+        }
+    }
+    Iterator nt = candles.iterator();
+    while(nt.hasNext()){
+        if(Integer.parseInt(nt.next().toString()) == currentTall){
+            numoftallest++;
+        }
+    }
+    return numoftallest;
+    }
+
+}
---
+++ birthdayCakeCandles/Solution.java
@@ -0,0 +1,56 @@
+package birthdayCakeCandles;
+/*
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. 
+They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+candles = [4,4,1,3]
+
+The maximum height candles are 4 units high. There are 2 of them, so return 2.
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+Returns
+
+int: the number of candles that are tallest
+Input Format
+
+The first line contains a single integer n, the size of candles[].
+The second line contains  space-separated integers, where each integer i describes the height of candles[i]. */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
