811ad338ba1fedd5fcfbe697a5c24005792b2e93
Umut <149551428+usakgun@users.noreply.github.com>
2024-07-21 03:44:59 +0200

    Add files via upload

--- breakingTheRecords/Result.java
+++ breakingTheRecords/Result.java
@@ -1,1 +1,44 @@
+package breakingTheRecords;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+    List<Integer> recordBreak = new ArrayList<>();
+    int maxCount = 0;
+    int minCount = 0;
+    int currentMaxRecord = scores.get(0);
+    int currentMinRecord = scores.get(0);
+    for(int i = 1;i<scores.size();i++){
+     if(scores.get(i) > currentMaxRecord){
+         currentMaxRecord = scores.get(i);
+         maxCount++;
+     } else if(scores.get(i) < currentMinRecord){
+         currentMinRecord = scores.get(i);
+         minCount++;
+     }
+    }
+    recordBreak.add(0, maxCount);
+    recordBreak.add(1, minCount);
+    return recordBreak;
+    }
+
+}
---
+++ breakingTheRecords/Solution.java
@@ -0,0 +1,70 @@
+package breakingTheRecords;
+/*
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. 
+She tabulates the number of times she breaks her season record for most points and least points in a game. 
+Points scored in the first game establish her record for the season, and she begins counting from there.
+
+Example
+scores = [12,24,10,24]
+
+Scores are in the same order as the games played. She tabulates her results as follows:
+
+                                     Count
+    Game  Score  Minimum  Maximum   Min Max
+     0      12     12       12       0   0
+     1      24     12       24       0   1
+     2      10     10       24       1   1
+     3      24     10       24       1   1
+Given the scores for a season, determine the number of times Maria breaks her records for most and least points scored during the season.
+
+Function Description
+
+Complete the breakingRecords function in the editor below.
+
+breakingRecords has the following parameter(s):
+
+int scores[n]: points scored per game
+Returns
+
+int[2]: An array with the numbers of times she broke her records. Index 0 is for breaking most points records, and index 1 is for breaking least points records.
+Input Format
+
+The first line contains an integer n, the number of games.
+The second line contains n space-separated integers describing the respective values of score0,score1,....,scoren-1.
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
