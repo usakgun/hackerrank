9f2ee1adcaad63b3918797de1f3c629efffd4a10
Umut <149551428+usakgun@users.noreply.github.com>
2024-07-21 03:37:49 +0200

    Add files via upload

--- gradingStudents/Result.java
+++ gradingStudents/Result.java
@@ -1,1 +1,42 @@
+package gradingStudents;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    List<Integer> result = new ArrayList<>();
+    
+    Iterator it = grades.iterator();
+    while(it.hasNext()){
+        int grade = Integer.parseInt(it.next().toString());
+        if (grade >= 38){
+            if(grade % 5 == 3){
+            grade = grade + 2;
+        } else if(grade % 5 == 4){
+            grade = grade + 1;
+        }
+        }
+        result.add(grade);
+    }
+    return result;
+    }
+
+}
---
+++ gradingStudents/Solution.java
@@ -0,0 +1,76 @@
+package gradingStudents;
+/*
+HackerLand University has the following grading policy:
+
+Every student receives a grade in the inclusive range from 0 to 100.
+Any grade less than 40 is a failing grade.
+
+Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+Examples
+
+ grade = 84 round to 85 (85 - 84 is less than 3)
+ grade = 29 do not round (result is less than 40)
+ grade = 57 do not round (60 - 57 is 3 or higher)
+
+Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
+
+Function Description
+
+Complete the function gradingStudents in the editor below.
+
+gradingStudents has the following parameter(s):
+
+int grades[n]: the grades before rounding
+Returns
+
+int[n]: the grades after rounding as appropriate
+Input Format
+
+The first line contains a single integer, , the number of students.
+Each line  of the  subsequent lines contains a single integer, grades[i]. */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
